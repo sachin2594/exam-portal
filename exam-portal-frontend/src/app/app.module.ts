00b95b0d1d34f04d8ba88510bfebdb4479b897fe
sachin.jadhav@techbulls.com <sachin.jadhav@techbulls.com>
2021-08-04 09:39:05 +0530

    added login component Home component created login form

--- exam-portal-frontend/src/app/app.module.ts
+++ exam-portal-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     NavbarComponent,
     FooterComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
